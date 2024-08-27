ad7c2fde47ed0a2f76c81d3a0f378124900ecace
DevDaniel22 <daniellovelock22@gmail.com>
2024-08-27 07:38:20 -0500

    Add files via upload

---
+++ 1.1.5SciFiName_Starter/NameMaker.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class NameMaker here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class NameMaker 
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class NameMaker
+     */
+    public NameMaker(String firstname, String lastname, String city, 
+    String school, String relativeName1, String relativeName2)
+    {
+      // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
